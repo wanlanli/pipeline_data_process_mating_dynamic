cc47e9cbd089b99a9b42a3ac41d8582fd2b3bc99
wlli <wanlanli.ky@gmail.com>
2024-04-08 12:47:16 +0200

    add time point p and m

--- script_wlli/mating_feature_extract.py
+++ script_wlli/mating_feature_extract.py
@@ -53,18 +53,34 @@ def extract_mating_features(image, g, key: int, granularity: int = 10):
     end_time = max(cells[c_mating.p].end, cells[c_mating.m].end)
 
     for flag, time in enumerate(range(end_time, start_time, -granularity)):
-        data = c_mating.center_cells_potential_pairs(time)
-        if data is not None:
-            if '-'+str(flag) not in feature_time_point.keys():
-                feature_time_point['-'+str(flag)] = None
-            feature_time_point['-'+str(flag)] = pd.concat([feature_time_point['-'+str(flag)], data])
+        feature_time_point = __set_single_time_point_feature(c_mating, feature_time_point,
+                                                             time, '-'+str(flag))
+        # data = c_mating.center_cells_potential_pairs(time)
+        # if data is not None:
+        #     if '-'+str(flag) not in feature_time_point.keys():
+        #         feature_time_point['-'+str(flag)] = None
+        #     feature_time_point['-'+str(flag)] = pd.concat([feature_time_point['-'+str(flag)], data])
 
+    # data = c_mating.center_cells_potential_pairs(start_time)
+    # if data is not None:
+    #     if 'start' not in feature_time_point.keys():
+    #         feature_time_point['start'] = None
+    #     feature_time_point["start"] = pd.concat([feature_time_point["start"], data])
+    feature_time_point = __set_single_time_point_feature(c_mating, feature_time_point,
+                                                         cells[c_mating.p].start, "start_p")
+    feature_time_point = __set_single_time_point_feature(c_mating, feature_time_point,
+                                                         cells[c_mating.m].start, "start_m")
+    return feature_time_point
+
+
+def __set_single_time_point_feature(c_mating, stack, start_time, name):
     data = c_mating.center_cells_potential_pairs(start_time)
     if data is not None:
-        if 'start' not in feature_time_point.keys():
-            feature_time_point['start'] = None
-        feature_time_point["start"] = pd.concat([feature_time_point["start"], data])
-    return feature_time_point
+        if name not in stack.keys():
+            stack[name] = data
+        else:
+            stack[name] = pd.concat([stack, data])
+    return stack
 
 
 def stack_features(all_data: dict = {}, data: dict = {}, name: str = "",  config=None):
